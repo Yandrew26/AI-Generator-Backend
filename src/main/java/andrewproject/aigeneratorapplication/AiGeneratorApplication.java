526d1a002c7aca067488f07821baa8c1245f51f2
Yandrew26 <andrewyan0226@gmail.com>
2025-01-23 15:49:54 -0800

    Created project

---
+++ src/main/java/andrewproject/aigeneratorapplication/AiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package andrewproject.aigeneratorapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/andrewproject/aigeneratorapplication/AiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package andrewproject.aigeneratorapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
