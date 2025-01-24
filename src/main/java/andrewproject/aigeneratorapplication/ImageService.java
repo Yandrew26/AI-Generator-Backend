a3e00a00b5588e9e08e16ccf7eea1ececa1561f3
Yandrew26 <andrewyan0226@gmail.com>
2025-01-23 18:33:35 -0800

    image generation options and parameterizing values

--- src/main/java/andrewproject/aigeneratorapplication/ImageService.java
+++ src/main/java/andrewproject/aigeneratorapplication/ImageService.java
@@ -3,6 +3,7 @@ package andrewproject.aigeneratorapplication;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,11 +15,33 @@ public class ImageService {
         this.openAiImageModel = openAiImageModel;
     }
 
-    public ImageResponse generateImage(String prompt) {
-        ImageResponse imageResponse = openAiImageModel.call(
-                new ImagePrompt(prompt)
-        );
+    public ImageResponse generateImage(String prompt,
+                                       String quality,
+                                       int n,
+                                       int width,
+                                       int height) {
+//        ImageResponse imageResponse = openAiImageModel.call(
+//                new ImagePrompt(prompt);
 
+//        ImageResponse imageResponse = openAiImageModel.call(
+//                new ImagePrompt(prompt,
+//                        OpenAiImageOptions.builder()
+//                                .withModel("dall-e-2")
+//                                .quality("hd")
+//                                .N(2)
+//                                .height(1024)
+//                                .width(1024).build())
+//        );
+
+        ImageResponse imageResponse = openAiImageModel.call(
+                new ImagePrompt(prompt,
+                        OpenAiImageOptions.builder()
+                                .withModel("dall-e-2")
+                                .quality(quality)
+                                .N(n)
+                                .height(height)
+                                .width(width).build())
+        );
         return imageResponse;
     }
 }
